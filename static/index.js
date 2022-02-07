d273560072d9869ec94cfafa5a0f1736e29f2630
0615xw <89594759+0615xw@users.noreply.github.com>
2022-02-07 21:37:32 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,10 +441,10 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '回去再练练？';
-        if (cps <= 8) return '肖外HP-1';
-        if (cps <= 10)  return '对方已受到致命下巴攻击';
-        if (cps <= 15) return '恭喜您成功打击肖外，凭截图获100软妹币！';
+        if (cps <= 2) return '回去再练练？';
+        if (cps <= 3) return '肖外HP-1';
+        if (cps <= 4)  return '对方已受到致命下巴攻击';
+        if (cps <= 5) return '恭喜您成功打击肖外，凭截图获100软妹币！';
         return '我美吗？';
     }
 
